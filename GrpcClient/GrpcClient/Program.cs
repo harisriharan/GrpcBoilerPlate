e2cf2b8a1b248dd8cd7d8a3f543e5b3de6d203c1
harisriharan <78483099+harisriharan@users.noreply.github.com>
2022-04-01 16:00:57 +0530

    Add files via upload
    Grpc Client

---
+++ GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7095");
+var client = new Clients.ClientsClient(channel);
+var reply = await client.CreateClientAsync(
+                  new ClientInfo { Name = "Hansuja" });
+Console.WriteLine("New Client Id: " + reply.Value);
+
+
+var clientInfo = await client.GetClientAsync(reply);
+Console.WriteLine("Data From DB: " + clientInfo.Name);
+
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
